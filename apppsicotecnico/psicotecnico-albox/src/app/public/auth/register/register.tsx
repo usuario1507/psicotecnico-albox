ae3cb07286ffccfe4a53e6e613a0f480d3f00557
Angel <angelgarcialopez15@gmail.com>
2024-06-20 10:08:04 +0200

    Co

--- apppsicotecnico/psicotecnico-albox/src/app/public/auth/register/register.tsx
+++ apppsicotecnico/psicotecnico-albox/src/app/public/auth/register/register.tsx
@@ -1,8 +1,14 @@
-import { useRouter } from "next/router";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../AuthContext";
-import { Link, input } from "@nextui-org/react";
-import Image from "next/image";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import { Button, Input } from "@nextui-org/react";
+
+
+
+
+
+
 
 
 
@@ -10,7 +16,6 @@ import Image from "next/image";
 
 
 interface IRespuestaRegister {
-  token: string;
   correo_electronico: string;
   contraseña: string;
   username: string;
@@ -20,56 +25,62 @@ type UsuarioData = {
   contraseña: string;
   username: string;
 };
-const Register = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Manejo del envío del formulario
-    // Puedes agregar la lógica para enviar los datos a tu API
-  };
+const RegisterPage = () => {
+  const router = useRouter();
+  const { registerUsuario } = useContext(AuthContext);
+  const { register, handleSubmit, formState: { errors } } = useForm<UsuarioData>();
+  const [ showError, setShowError ] = useState(false);
+  const [ errorMessage, setErrorMessage ] = useState('');
+
+  const onRegisterUsuario = async (InputData: UsuarioData) => {
+    setShowError(false);
+    const { correo_electronico, contraseña, username } = InputData;
+    const {hasError, message } = await registerUsuario(correo_electronico, contraseña, username)
+    console.log(message);
+    if (hasError){
+      setShowError(true);
+      setErrorMessage(message || '');
+      setTimeout( () => setShowError(false), 3001)
+      return;
+    }
+
+    router.replace('/');
+  }
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
-          Flowbite
-        </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Create an account
-            </h1>
-            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
-              </div>
-              <div>
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-              </div>
-              <div>
-                <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-              </div>
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                </div>
-              </div>
-              <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account? <Link href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
-              </p>
-            </form>
-          </div>
-        </div>
+    <div>
+      <h1 className="container text-2xl mx-auto mt-4">Formulario de Medico</h1>
+      <form 
+          onSubmit={ handleSubmit(onRegisterUsuario)}  noValidate
+          className="flex flex-row flex-wrap"    
+      >
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                  </div>
+                  <div>
+                      <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                      <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                  </div>
+                  <div class="flex items-start">
+                      <div class="flex items-center h-5">
+                        <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                      </div>
+                      <div class="ml-3 text-sm">
+                        <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                      </div>
+                  </div> 
+      <Link  color="foreground" passHref  href="/public/auth/register" >
+       <Button type="submit" color="primary" className="container mx-auto mt-7">Enviar</Button>
+      </Link>
       </div>
-    </section>
-  );
-};
+      </form>
+    </div>
+  )
+}
 
-export default Register;
+export default RegisterPage;
